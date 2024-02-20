249e2281eb8452effbe1405405e1ecf06d3eaf43
danehobrecht <76x5l22l@anonaddy.me>
2024-02-19 18:29:22 -0800

    modified:   angeline.py

--- angeline.py
+++ angeline.py
@@ -1,4 +1,4 @@
-### Let the Lord remind me, day in and day out, that this program is for Him alone.
+### Let the Ghost remind me, day in and day out, that this program is for Him alone.
 
 import re
 import json
