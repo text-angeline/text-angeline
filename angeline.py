b13c9985b20e61fb55fb6cacb47ffa09530d8d90
danehobrecht <76x5l22l@anonaddy.me>
2024-02-20 22:53:24 -0800

    modified:   .gitignore 	modified:   angeline.py

--- angeline.py
+++ angeline.py
@@ -100,7 +100,7 @@ def send_message(message_protocol, text_content, user_number):
         to=user_number,
         text=text_content,
         type_=message_protocol,
-    )
+    ) 
 
 ### Throw error message
 def throw_error(error_content, user_number):
@@ -188,7 +188,7 @@ def fetch_text(bible, unit, query, user_number):
     url = f"https://api.scripture.api.bible/v1/bibles/{bible}/{unit}/{query}?content-type=json&include-notes=false&include-titles=true&include-chapter-numbers=false&include-verse-numbers=true&include-verse-spans=false"
     headers = {"api-key": API_BIBLE_KEY}
     api_bible_response = requests.request("GET", url, headers=headers)
-    print(api_bible_response.text)
+    # print(api_bible_response.text)
     api_bible_data = api_bible_response.json()
 
     # Text extraction/delivery
@@ -228,4 +228,4 @@ def fetch_text(bible, unit, query, user_number):
         throw_error("Couldn't find the text", user_number)
 
 # Allow development run (uncomment):
-init_dev()
+# init_dev()
