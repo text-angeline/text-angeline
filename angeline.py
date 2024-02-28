9c4387b167da3cf1adbbd85bb640ef0d0a0d6bd2
danehobrecht <76x5l22l@anonaddy.me>
2024-02-27 21:51:45 -0800

    modified:   angeline.py

--- angeline.py
+++ angeline.py
@@ -96,7 +96,7 @@ book_dict = {
 ### Send text message
 def send_message(message_protocol, text_content, user_number):
 	# Allow development halt (uncomment):
-	return
+	# return
 	telnyx.Message.create(
 		from_=TELNYX_NUMBER,
 		to=user_number,
@@ -226,4 +226,4 @@ def fetch_text(bible, unit, query, user_number):
 	send_message(message_protocol, text_content, user_number)
 
 # Allow development run (uncomment):
-init_dev()
+# init_dev()
