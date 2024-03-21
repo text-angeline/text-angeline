65c7a738a370c7e471857230558014f15c690635
danehobrecht <76x5l22l@anonaddy.me>
2024-03-20 19:45:54 -0700

    modified:   angeline.py

--- angeline.py
+++ angeline.py
@@ -11,11 +11,13 @@ import xml.etree.ElementTree as ET
 with open("config.json", 'r') as file:
     config = json.load(file)
 
-### Constants
+### Global variables
+# Telnyx
+telnyx.api_key = config["TELNYX_KEY"]
+# Constants
 SMS_MAX_CAP = 160
 MMS_MAX_CAP = 1600
 GITHUB_TOKEN = config["GITHUB_TOKEN"]
-telnyx.api_key = config["TELNYX_KEY"]
 DEFAULT_TRANS = config["DEFAULT_TRANS"]
 TELNYX_NUMBER = config["TELNYX_NUMBER"]
 
