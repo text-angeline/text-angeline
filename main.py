1d51fb55d0896b04f6f382f71fbd589af6135e97
danehobrecht <76x5l22l@anonaddy.me>
2024-03-20 19:48:13 -0700

    modified:   main.py

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ def receive_sms(request, context):
     print(f"Request: {request}")
     # print(f"Context: {context}")
     try:
-        event_type = request.get('event_type')
-        if (event_type == 'message.received'):
-            user_input = bleach.clean(request['payload']['text'])
-            user_number = request['payload']['from']['phone_number']
+        event_type = request.get("event_type")
+        if (event_type == "message.received"):
+            user_input = bleach.clean(request["payload"]["text"])
+            user_number = request["payload"]["from"]['phone_number"]
             print("user_input:", user_input)
             print("user_number:", user_number)
             angeline.init(user_input, user_number)
             return '', 200
     except KeyError as e:
-        print("Error: Couldn't process webhook:", e)
+        print("Error: Couldn't process webhook.", e)
         return '', 400
